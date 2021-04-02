58b2367c008f738d5d60c19fe7cd1c0c38d80fdd
Jennifer Parrett <jennifer.parrett@comcast.net>
2021-04-02 13:56:34 -0400

    deliverable2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -26,3 +26,22 @@ INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY count DESC;
+
+--Deliverable Two
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+    e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM dept_emp AS de
+JOIN employees AS e
+ON (de.emp_no = e.emp_no)
+JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01')
+	   AND (t.to_date = '9999-01-01')
+	   AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
